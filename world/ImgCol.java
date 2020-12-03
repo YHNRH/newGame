2a26f9f2234a43f867d226d28426ee19619c8920
YHNRH <kdog2105@mail.ru>
2020-12-03 13:48:33 +0300

    Загрузка картинок персонажа

--- world/ImgCol.java
+++ world/ImgCol.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 
 public class ImgCol {
     public static final BufferedImage ground;
+    public static final BufferedImage[][] player = new BufferedImage[3][2];
+
     static {
 	BufferedImage _ground;
 	try {
@@ -17,5 +19,28 @@ public class ImgCol {
 		  _ground = null;
 	      }
 	ground = _ground;
+	BufferedImage player_full;
+	try
+	    {
+		int i = 0;
+		player_full = ImageIO.read(new File("res/pers.png"));
+		for (int stolb=292;stolb<421;stolb+=43)
+		    {
+			int j=0;
+			for (int stroka = 101; stroka<239; stroka+=69)
+			    {
+				player[i][j] = player_full.getSubimage(stolb, stroka, 43, 69);
+				j++;
+		    }
+			i++;
+		    }
+	    }
+	catch (IOException e)
+	    {
+		player_full = null;
+		e.printStackTrace();
+        }
+	
     }
+    
 }
